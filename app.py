c7aee0e1935f5a3a07c1cb37344df7f6cdb7e673
Tobi DEGNON <tobidegnon@proton.me>
2023-11-21 19:27:02 +0100

    configurable static url

--- app.py
+++ app.py
@@ -3,9 +3,10 @@
 from django.core.management import execute_from_command_line
 
 from coltrane import initialize
+from os import getenv
 
 
-wsgi = initialize()
+wsgi = initialize(**{"STATIC_URL": getenv("STATIC_URL", "static/")})
 
 if __name__ == "__main__":
     execute_from_command_line()

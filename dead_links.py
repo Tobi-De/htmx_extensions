27529f673f51be6de71d36c8cacdc688f39d1313
Tobi DEGNON <tobidegnon@proton.me>
2023-11-22 15:30:14 +0100

    CI: frequently check for dead links

--- dead_links.py
+++ dead_links.py
@@ -1,7 +1,12 @@
 import json
 from pathlib import Path
 import urllib.request
-import threading
+import concurrent.futures
+import os
+import datetime as dt
+
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+REPO = "Tobi-De/htmx_extensions"
 
 
 def check_link(url):
@@ -9,8 +14,10 @@ def check_link(url):
         urllib.request.urlopen(url)
     except urllib.error.HTTPError as e:
         print(f"HTTPError: {url} {e.code}")
+        return url
     except urllib.error.URLError as e:
         print(f"URLError: {url} {e.reason}")
+        return url
     else:
         print(f"OK: {url}")
 
@@ -19,19 +26,37 @@ def check_dead_links():
     extensions = Path("data/extensions.json")
     extensions = extensions.read_text()
     extensions = json.loads(extensions)
-    threads = []
-    for extension in extensions.values():
-        doc_url = extension["doc_url"]
-        download_url = extension["download_url"]
-        thread1 = threading.Thread(target=check_link, args=(doc_url,))
-        thread2 = threading.Thread(target=check_link, args=(download_url,))
-        thread1.start()
-        thread2.start()
-        threads.append(thread1)
-        threads.append(thread2)
-    for thread in threads:
-        thread.join()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_to_url = {
+            executor.submit(check_link, url): url
+            for extension in extensions.values()
+            for url in (extension["doc_url"], extension["download_url"])
+        }
+    dead_links = [
+        future.result()
+        for future in concurrent.futures.as_completed(future_to_url)
+        if future.result() is not None
+    ]
+    return dead_links
+
+
+def create_github_issue(dead_links):
+    url = f"https://api.github.com/repos/{REPO}/issues"
+    headers = {
+        "Authorization": f"token {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github.v3+json",
+    }
+    data = {
+        "title": f"Dead links - {dt.datetime.now().strftime('%Y-%m-%d %H:%M')}",
+        "body": "\n".join(dead_links),
+    }
+    req = urllib.request.Request(url, json.dumps(data).encode(), headers)
+    urllib.request.urlopen(req)
+  
+
 
 
 if __name__ == "__main__":
-    check_dead_links()
+    dead_links = check_dead_links()
+    if dead_links:
+        create_github_issue(dead_links)
